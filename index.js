dd0412a411b92821e0c8a41615fb85c5c880ce35
t <tysonpederson95@gmail.com>
2023-01-17 14:30:03 -0600

    Clean up formatting of weather data returned from processWeatherData

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 const apiKey = 'ef3d247a1b61d1160024768943d53ab9';
 
 const search = document.getElementById('search');
+const input = document.getElementById('city');
 search.addEventListener('click', main);
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter'){
+    input.blur();
+    main();
+  }
+});
 
 async function main() {
-  // Assign variables based on user's search
-  let city = document.getElementById('city').value;
-  let state = document.getElementById('state').value;
-  let country = document.getElementById('country').value;
-  if (city == '' || state == '' || country == '') {
-    console.log('unknown location');
-    return;
-  }
+  const city = document.getElementById('city').value;
   const units = 'imperial';
-  const geocoding = await getGeocoding(city, state, country, apiKey);
-  // Reassign location variables based on geocoding response to keep information uniform when displaying
-  city = geocoding[0].name;
-  state = geocoding[0].state;
-  country = geocoding[0].country;
+  const geocoding = await getGeocoding(city, apiKey);
   let weatherData = await getWeatherData(geocoding, units, apiKey);
-  weatherData = processWeatherData(weatherData, city, state, country);
+  weatherData = processWeatherData(weatherData);
   console.log(weatherData);
 }
 
+async function getGeocoding(city, apiKey) {
+  let r = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`);
+  r = await r.json();
+  return {
+    lat: r[0].lat,
+    lon: r[0].lon,
+  };
+}
+
 async function getWeatherData(geocoding, units, apiKey) {
-  const lat = geocoding[0].lat;
-  const lon = geocoding[0].lon;
+  const lat = geocoding.lat;
+  const lon = geocoding.lon;
   r = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`);
   return await r.json();
 }
 
-async function getGeocoding(city, state, country, apiKey) {
-  let r = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=${apiKey}`);
-  return await r.json();
-}
-
-function processWeatherData(weatherData, city, state, country) {
-  weatherType = weatherData.weather[0].main;
+function processWeatherData(weatherData) {
+  weatherType = weatherData.weather[0].description;
   temp = weatherData.main.temp;
-  return new Weather(weatherType, temp, city, state, country);
-}
-
-class Weather {
-  constructor(weatherType, temp, city, state, country) {
-    this.weatherType = weatherType;
-    this.temp = temp;
-    this.city = city;
-    this.state = state;
-    this.country = country;
-  }
+  feelsLike = weatherData.main.feels_like;
+  humidity = weatherData.main.humidity;
+  city = weatherData.name;
+  return {
+    weatherType,
+    temp,
+    feelsLike,
+    humidity,
+    city
+  };
 }
